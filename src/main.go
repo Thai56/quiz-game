2220dedb47ff47e83afa0d3e59adc8b62c4f8797
Thai Tran <thai.tran@TCN-10418.local>
2018-08-26 17:39:36 -0600

    now reading csv file

---
+++ src/main.go
@@ -0,0 +1,46 @@
+//package main
+package main
+
+import (
+	"encoding/csv"
+  "bufio"
+	"fmt"
+	"log"
+  "os"
+  "strings"
+  //"reflect"
+)
+
+func main() {
+  filePath := os.Args[1]
+
+  csvFile, _ := os.Open(filePath)
+
+  record := csv.NewReader(bufio.NewReader(csvFile))
+
+
+  problems, err := record.ReadAll()
+
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  counter :=  0
+
+  for _, p := range problems {
+    reader := bufio.NewReader(os.Stdin)
+    problem, answer := p[0], p[1]
+
+    fmt.Print("What is", problem, " Sir? \n")
+
+    text, _ := reader.ReadString('\n')
+    userAnswer := strings.TrimSuffix(text, "\n")
+
+    if userAnswer != answer {
+      counter++
+    }
+  }
+
+  fmt.Println("Got ", counter, " wrong!")
+}
+
