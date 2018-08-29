6701df912c176dcab4d96539d53bf9cf57aa6104
Thai Tran <thai.tran@TCN-10418.local>
2018-08-29 01:22:00 -0600

    clean code and timer working with quiz game

--- src/main.go
+++ src/main.go
@@ -9,64 +9,88 @@ import (
   "os"
   "strings"
   "flag"
-  //"reflect"
   "path/filepath"
+  "time"
+  "io"
 )
 
-func main() {
-  csvPath := flag.String("csv", "problems.csv", "will be a string")
 
-  // TODO: Use the timer flag below to create a timer
-  timer := flag.Int("timer", 30, "timer quiz assessment")
-
-  // TODO: Find a way to refactor below
-
-  flag.Parse()
-
-  fmt.Println("timer ", *timer)
-
-  pwd, _ := os.Getwd()
-
-  csvFile, _ := filepath.Abs(pwd + "/" + *csvPath)
-
-  fileContents, _ := os.Open(csvFile)
-
-  record := csv.NewReader(bufio.NewReader(fileContents))
-
-  problems, err := record.ReadAll()
-
-  fmt.Println("problems ", problems)
-
-  if err != nil {
-    log.Fatal(err)
-  }
-
-  counter :=  0
-
-  for i, p := range problems {
-    reader := bufio.NewReader(os.Stdin)
-    problem, answer := p[0], p[1]
-
-    fmt.Print(i + 1, ".) What is ", problem, " Sir? \n")
-
-    text, _ := reader.ReadString('\n')
-    userAnswer := strings.TrimSuffix(text, "\n")
-
-    if userAnswer != answer {
-      counter++
-    }
-  }
-
-  correctCount := (len(problems) - counter);
-  fmt.Println("Got ", correctCount, "/", len(problems), " right!")
-  /*
-   *TODO: Part Two:Adapt your program from part 1 to add a timer. The default time limit should be 30 seconds, but should also be customizable via a flag.
-
-   Your quiz should stop as soon as the time limit has exceeded. That is, you shouldn’t wait for the user to answer one final questions but should ideally stop the quiz entirely even if you are currently waiting on an answer from the end user.
-
-
-   */
-
-
+type Problem struct {
+  Question string
+  Answer string
 }
 
+func getProblems(problems *[]Problem, problemsDone chan bool) {
+  csvPath := flag.String("csv", "problems.csv", "will be a string")
+  flag.Parse()
+  pwd, _ := os.Getwd()
+  csvFile, _ := filepath.Abs(pwd + "/" + *csvPath)
+  fileContents, _ := os.Open(csvFile)
+  reader := csv.NewReader(bufio.NewReader(fileContents))
+
+  for {
+    line, error := reader.Read()
+    if error == io.EOF {
+      break
+    } else if error != nil {
+      log.Fatal(error)
+    }
+
+    *problems = append(*problems, Problem{
+      Question: line[0],
+      Answer: line[1],
+    })
+  }
+  problemsDone <- true
+
+  close(problemsDone)
+}
+
+func startQuizGame(problems []Problem, quizDone chan bool, correctCount *int) {
+  for i, p := range problems {
+    reader := bufio.NewReader(os.Stdin)
+    fmt.Print(i + 1, ".) What is ", p.Question, " Sir? \n")
+    text, _ := reader.ReadString('\n')
+    userAnswer := strings.TrimSuffix(text, "\n")
+    if userAnswer == p.Answer {
+      *correctCount++
+    }
+  }
+  quizDone <- true
+
+  close(quizDone)
+}
+
+func main() {
+  timerFlag := flag.Int("timer", 15, "timer quiz assessment")
+
+  // Gathering Problems
+  var problems []Problem
+  var problemsDone = make(chan bool)
+  go getProblems(&problems, problemsDone)
+  <-problemsDone
+
+  // Configuring Timer
+  timeInSeconds := int32(*timerFlag)
+  timer1 := time.NewTimer(time.Duration(timeInSeconds) * time.Second)
+
+  // Starting Quiz
+  var quizOver = make(chan bool)
+  correctCount := 0
+  go startQuizGame(problems, quizOver, &correctCount)
+
+  fmt.Println("quiz starting...")
+
+  for {
+    select {
+    case <-timer1.C:
+      fmt.Println("\nTime ran out!...")
+      fmt.Println("Got ", correctCount, "/", len(problems), " right!")
+      return
+    case <-quizOver:
+      fmt.Println("Congradulations! You finished the quiz within the set time!...")
+      fmt.Println("Got ", correctCount, "/", len(problems), " right!")
+      return
+    }
+  }
+}
